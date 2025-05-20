6c03244832318d7da5c492b74c68b3800e830851
xiulinyang <xiulin.yang.compling@gmail.com>
2025-05-20 11:02:55 -0400

    update tokenizer analysis script to count characters tokens and bytes

--- tokenizer_test.py
+++ tokenizer_test.py
@@ -14,19 +14,26 @@ args = parser.parse_args()
 lang = args.lang
 vocab_size = args.vocab_size
 
-# for language in tqdm(TOKENIZER_DICT):
-tokenizer = AutoTokenizer.from_pretrained(f'models/{lang}_{vocab_size}_41')
-sents = Path(f'/scratch/xiulyang/multilingual-LM/data/multilingual/{lang}/test/{lang}.test').read_text().strip().split('\n')
-token_num =0
-bits_num =0
-character_num =0
-for sent in tqdm(sents):
-    tokens = tokenizer.tokenize(sent)
-    token_num += len(tokens)
-    bits_num+=len(sent.encode('utf-8'))
-    character_num+=len(sent)
+languages = ['AR', 'DE', 'EN', 'TR', 'RU']
+vocab_sizes = ['200', '5000', '10000' ,'20000', '30000', '50000']
+with open('counts.tsv', 'w') as counts:
+    counts.write('language\tvocab_size\tcorpus_token_counts\tbit_counts\tcharacter_counts\n')
+    for lang in tqdm(languages):
+        for vocab_size in tqdm(vocab_sizes):
+            tokenizer = AutoTokenizer.from_pretrained(f'models/{lang}_{vocab_size}_41')
+            sents = Path(f'/scratch/xiulyang/multilingual-LM/data/multilingual/{lang}/test/{lang}.test').read_text().strip().split('\n')
+            token_num =0
+            bits_num =0
+            character_num =0
+            for sent in tqdm(sents):
+                tokens = tokenizer.tokenize(sent)
+                token_num += len(tokens)
+                bits_num+=len(sent.encode('utf-8'))
+                character_num+=len(sent)
 
-with
-average =token_num/len(sents)
-print(lang, average)
-print(token_num)
+            counts.write(f'{lang}\t{vocab_size}\t{token_num}\t{bits_num}\t{character_num}\n')
+
+
+# average =token_num/len(sents)
+# print(lang, average)
+# print(token_num)
